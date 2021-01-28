ff0f29c1fd0f7fdbadafe181cd15af50341f4e03
ikzath kalanthar <ikzathjob@hotmail.com>
2021-01-28 23:05:00 +0100

    commit

--- server.js
+++ server.js
@@ -37,9 +37,9 @@ if(process.env.NODE_ENV === 'production') {
      // serve static folder
        app.use(express.static('client/build'));
      
-     //   app.get('*', (req, res)=> {
-     //        res.sendFile(path.resolve(__dirname, 'client', 'mern-stack', 'build', 'index.html'));
-     //   } ); 
+       app.get('*', (req, res)=> {
+            res.sendFile(path.resolve(__dirname, 'client', 'mern-stack', 'build', 'index.html'));
+       } ); 
      }
      
 
