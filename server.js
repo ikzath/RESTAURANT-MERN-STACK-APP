f75fb700141af0165775aa39fa2de436c23094a9
ikzath kalanthar <ikzathjob@hotmail.com>
2021-01-29 00:15:05 +0100

    commit

--- server.js
+++ server.js
@@ -35,10 +35,10 @@ app.use('/api/items', items);
 // serve static assets  if in production
 if(process.env.NODE_ENV === 'production') {
      // serve static folder
-       app.use(express.static('client/build'));
+       app.use(express.static('client/mern-stack/build'));
      
        app.get('*', (req, res)=> {
-            res.sendFile(path.resolve(__dirname, 'client', 'mern-stack', 'build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, 'client/mern-stack/build/index.html'));
        } ); 
      }
      
