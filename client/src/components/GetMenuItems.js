6310cf17572b528e5bcbd4df19f596477272db3a
ikzath kalanthar <ikzathjob@hotmail.com>
2021-03-23 23:18:05 +0100

    'cors'

--- client/src/components/GetMenuItems.js
+++ client/src/components/GetMenuItems.js
@@ -9,14 +9,9 @@ function GetMenuItems() {
 
     const [data, setData] = useState([]); 
 
-    const instance = axios.create({
-      // .. where we make our configurations
-          baseURL: 'http://localhost:6600/api/items'
-      });
-
     useEffect(() => {
         axios
-        .get(instance, { headers: {"Access-Control-Allow-Origin": "*"}} )
+        .get('http://localhost:6600/api/items')
         .then(res => {
           setData(res.data)
           console.log(data);
